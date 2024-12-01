f179df8c1d1b75785c78bf0d7a290e1a777c0562
webb-wanderer <140278726+webb-wanderer@users.noreply.github.com>
2024-12-01 10:41:05 -0800

    move DH nav section

--- _assets/js/footer-js.js
+++ _assets/js/footer-js.js
@@ -1,3 +1,4 @@
 $( "#nav-dh" ).load( "/_assets/includes/toc.htm" );
 $( "#nav-hhp" ).load( "/_assets/includes/nav-hhp.htm" );
+$( "#nav-dh" ).load( "/_assets/includes/nav-dh.htm" );
 $( "#footer" ).load( "/_assets/includes/footer.htm" );
