b2f36e7698576269bf2c1bf15c2191ca2956cebf
webb-wanderer <140278726+webb-wanderer@users.noreply.github.com>
2024-12-02 05:34:56 -0800

    normalize files and add search

---
+++ _assets/js/footer-js-min.js
@@ -0,0 +1,1 @@
+$("#nav-dh").load("/toc.htm"),$("#nav-hhp").load("/_assets/includes/nav-hhp.htm"),$("footer").load("/_assets/includes/footer.htm");
--- _assets/js/footer-js.js
+++ _assets/js/footer-js.js
@@ -1,4 +1,3 @@
-$( "#nav-dh" ).load( "/_assets/includes/toc.htm" );
+$( "#nav-dh" ).load( "/toc.htm" );
 $( "#nav-hhp" ).load( "/_assets/includes/nav-hhp.htm" );
-$( "#nav-dh" ).load( "/_assets/includes/nav-dh.htm" );
-$( "#footer" ).load( "/_assets/includes/footer.htm" );
+$( "footer" ).load( "/_assets/includes/footer.htm" );
